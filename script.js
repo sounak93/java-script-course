a1dfb54e506a70d6d2bdd8a15268e3b244036e94
Sounak De <sounakde1993@gmail.com>
2025-10-09 18:28:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//Arithmetic operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a =", a , " $ b =" , b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+
+// Unary Operator
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a =", a , " $ b =" , b);
+
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+
+
+// Assignment Operators
+
+let a = 5;
+let b = 2;
+
+a **= 4; // a = a ** 4
+console.log(a);
