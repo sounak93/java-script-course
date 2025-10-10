2f1442185750c548dfd48c91737bdd1d58ccf38c
Sounak De <sounakde1993@gmail.com>
2025-10-10 18:27:12 +0530

    second commit

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
-//Arithmetic operators
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Sounak De")
+    
+// }
 
-// let a = 5;
-// let b = 2;
+// let sum = 0;
+// let n = 100;
+// for(let i=1;i<=n;i++){
+//     sum = sum + i;
+// }
 
-// console.log("a =", a , " $ b =" , b);
-// console.log("a + b =", a + b);
-// console.log("a - b =", a - b);
-// console.log("a * b =", a * b);
-// console.log("a / b =", a / b);
-// console.log("a % b =", a % b);
-// console.log("a ** b =", a ** b);
+// console.log(sum);
 
-// Unary Operator
+// let i = 1;
 
-// let a = 5;
-// let b = 2;
+// while(i <= 10) {
+//    console.log("Sounak");
+//    i++;
+// }
 
-// console.log("a =", a , " $ b =" , b);
+// let i = 1;
 
-// console.log("a-- = ", a--);
-// console.log("a = ", a);
+// do {
+//     console.log('i =',i);
+//     i++;
+// } while (i<= 10);
 
+// let str = "Javascript";
 
-// Assignment Operators
+// let size = 0
+// ;
+// for(let val of str) {
+//     console.log("val =", val);
+//     size++;
+// }
 
-let a = 5;
-let b = 2;
+// console.log("Size of the string: ", size);
 
-a **= 4; // a = a ** 4
-console.log(a);
+// let student = {
+//     name: "Rahul Kumar",
+//     age: 24,
+//     cgpa: 8.2,
+//     isPass: true
+// };
+
+// for(let key in student) {
+//     console.log('key =', key, 'value =', student[key]);
+// }
+
+//Print all the even numbers for 0-100
+
+// for(let i=0;i<=100;i++) {
+//     if(i%2 !== 0) {
+//         console.log(i);
+
+//     }
+// }
+
+// Q2. Create a game where you start a game with any random game number.Ask the user to keep gussing the game number until the user enters correct value
+
+let num = 25;
+let userNum = prompt("Guess the game number : ")
+
+while(userNum != num) {
+prompt("You have entered the wrong number.Guess again :")
+}
+
+console.log("Congratulation! You have entered the right number.")
